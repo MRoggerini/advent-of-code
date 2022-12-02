76858183040a099887e9b75e71060186ba974359
Marco Roggerini <marco.roggerini@gmail.com>
2022-12-02 20:34:06 +0100

    add comments and cleaning of code

--- 2022/day2.py
+++ 2022/day2.py
@@ -1,19 +1,3 @@
-
-move = {
-        'A': 0,
-        'B': 1,
-        'C': 2,
-        'X': 0,
-        'Y': 1,
-        'Z': 2
-    }
-
-point_outcome: {
-        'X': 0,
-        'Y': 3,
-        'Z': 6
-    }
-
 # rows: our play
 # columns: their play
 # cell: the score of the selected play
@@ -35,20 +19,28 @@ game_matrix_pt2 = [
     ]
 
 
+# get the input data
 with open('day2_input.txt') as f:
     data = f.readlines()
 
 score_pt1 = []
+tot_score_pt1 = 0
 score_pt2 = []
+tot_score_pt2 = 0
 
 for i in data:
-    col1 = move[i[0]] # the move of our challenger
-    col2 = move[i[2]] # pt1: our move; pt2: the outcome to obtain
+    col1 = move_score[i[0]] # the move of our challenger
+    col2 = move_score[i[2]] # pt1: our move; pt2: the desired outcome
 
+    # we get the score of the strategy in pt1
     curr_score_pt1 = game_matrix_pt1[col2][col1]
-    curr_score_pt2 = game_matrix_pt2[col2][col1]
     score_pt1.append(curr_score_pt1)
-    score_pt2.append(curr_score_pt2)
+    tot_score_pt1 += curr_score_pt1
 
-print(f'grand total with part 1 strategy: {sum(score_pt1)}')
-print(f'grand total with part 2 strategy: {sum(score_pt2)}')
+    # we get the score of the strategy in pt2
+    curr_score_pt2 = game_matrix_pt2[col2][col1]
+    score_pt2.append(curr_score_pt2)
+    tot_score_pt2 += curr_score_pt2
+
+print(f'grand total with part 1 strategy: {tot_score_pt1}')
+print(f'grand total with part 2 strategy: {tot_score_pt2}')
