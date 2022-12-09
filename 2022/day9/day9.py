e40a84717c521d072e22c6a47334bf6ed65b6b9b
Marco Roggerini <m.roggerini@reply.it>
2022-12-09 21:46:55 +0100

    fix code to solve challenge of day 9

--- 2022/day9/day9.py
+++ 2022/day9/day9.py
@@ -10,35 +10,64 @@ def move_left(position):
 def move_right(position):
     position[1] += 1
 
-def stay(position):
-    pass
-
 # move defines a compact way to represent the possible movements and apply them
 move = {
         'U': move_up,
         'D': move_down,
         'L': move_left,
-        'R': move_right,
-        '0': stay
+        'R': move_right
     }
 
+def move_t(val, dist):
+    # how to move knot based on movement of previous knot
+    # we both do hotizontal and vertical movement in each case:
+    # if both distance is > 0, we certainly need to do a diagonal movement
+    # else we will only operate an horizontal/vertical movement
+    if dist[0] > 0:
+        move['D'](val)
+    elif dist[0] < 0:
+        move['U'](val)
+
+    if dist[1] > 0:
+        move['R'](val)
+    elif dist[1] < 0:
+        move['L'](val)
 
 
 with open('day9_input.txt') as f:
     data = f.read().splitlines()
 
-visited_tail = set()
-h_pos = [0, 0] # [row, col]
-t_pos = [0, 0] # [row, col]
+num_knots = 10
+visited = {i:set() for i in range(num_knots)}
+rope_pos = {i:[0,0] for i in range(num_knots)} # [row, col]
+# add starting position as visited for all knots
+for k,v in visited.items():
+    v.add('0:0')
 
 for i in data:
     direction = i[0]
     cardinality = int(i[2:])
     for j in range(cardinality):
-        move[direction](h_pos)
-        curr_dist = [h_pos[0] - t_pos[0], h_pos[1] - t_pos[1]]
-        manh_dist = abs(curr_dist[0]) + abs(curr_dist[1])
-        if max(abs(curr_dist[0]), abs(curr_dist[1])) > 1:
-            if manh_dist == 2:
+        for k, val in rope_pos.items():
+            if k == 0: # head always move
+                move[direction](val)
+                visited[k].add(f'{val[0]}:{val[1]}')
+            else:
+                # other knots only move if previous knot has a Cebysev distance
+                # greater than 1
+                # Cebysev distance: the king's distance, the number of moves a
+                # king in chess must do to move from A to B
+                curr_dist = [
+                        rope_pos[k-1][0] - rope_pos[k][0],
+                        rope_pos[k-1][1] - rope_pos[k][1]
+                    ]
+                if max(abs(curr_dist[0]), abs(curr_dist[1])) > 1:
+                    move_t(val, curr_dist)
+                    visited[k].add(f'{val[0]}:{val[1]}')
+                else:
+                    # if this knot did not move, the next one will not move,
+                    # since it is already ad Cebysev distance <= 1
+                    break
 
-print(h_pos)
+for k, v in visited.items():
+    print(f'Locations visited by knot {k}: {len(v)}')
